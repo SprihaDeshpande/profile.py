f3a868c926f26fff84d04e2fc58b57c088f357a2
Spriha Deshpande <50254090+SprihaDeshpande@users.noreply.github.com>
2024-09-26 21:52:56 -0700

    Create about_me.py

---
+++ about_me.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+# Inject custom CSS for the About Me page
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: lightgray;
+    }
+    .about-me-section {
+        margin-top: 50px;
+        text-align: center;
+        color: black;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# About Me section
+st.markdown(
+    """
+    <div class="about-me-section">
+        <h1>About Me</h1>
+        <p>Here is some information about me...</p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
